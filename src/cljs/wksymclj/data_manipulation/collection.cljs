fd3f36d99e831dcc283fecc0649bd582b2afa8d4
whacked <whacked@users.noreply.github.com>
2018-04-30 00:20:44 -0700

    Move general collection functions to data manip

---
+++ src/cljs/wksymclj/data_manipulation/collection.cljs
@@ -0,0 +1,40 @@
+(ns wksymclj.data-manipulation.collection)
+
+;; #+pure
+(defn is-subvector?
+  "`needle` and `haystack` are seq-able types
+  returns index where `needle` is first found in `haystack`
+  if exists, else returns nil"
+  [needle haystack]
+  (let [i-hs-max (inc (- (count haystack) (count needle)))
+        i-nd-max (count needle)
+        ]
+    (loop [i-hs 0]
+      (if (< i-hs i-hs-max)
+        (let [match-length
+              (loop [i-nd 0]
+                (let [i-hs-combined (+ i-hs i-nd)]
+                  (if (= i-nd i-nd-max)
+                    i-nd
+                    (let [nd-val (nth needle i-nd)
+                          hs-val (nth haystack i-hs-combined)]
+                      (if (= nd-val hs-val)
+                        (recur (inc i-nd)))))))]
+          (if match-length
+            i-hs
+            (recur (+ i-hs (or match-length 1)))))))))
+
+;; #+pure
+(defn get-proper-subset-keys [base comp]
+  (filter (set (keys base))
+          (keys comp)))
+
+;; #+pure
+(defn is-submap? [smaller bigger]
+  (let [required-keys (get-proper-subset-keys smaller bigger)]
+    (and (= (count smaller)
+            (count required-keys))
+         (every? identity
+                 (map #(= (smaller %) (bigger %))
+                      required-keys)))))
+
--- src/cljs/wksymclj/data_manipulation/graph.cljs
+++ src/cljs/wksymclj/data_manipulation/graph.cljs
@@ -1,4 +1,6 @@
-(ns wksymclj.data-manipulation.graph)
+(ns wksymclj.data-manipulation.graph
+  (:require [wksymclj.data-manipulation.collection
+             :as wk-coll]))
 
 ;; #+pure
 (defn get-edge-midpt [pt-seq]
@@ -55,48 +57,10 @@
         (apply concat))})
 
 ;; #+pure
-(defn is-subvector?
-  "`needle` and `haystack` are seq-able types
-  returns index where `needle` is first found in `haystack`
-  if exists, else returns nil"
-  [needle haystack]
-  (let [i-hs-max (inc (- (count haystack) (count needle)))
-        i-nd-max (count needle)
-        ]
-    (loop [i-hs 0]
-      (if (< i-hs i-hs-max)
-        (let [match-length
-              (loop [i-nd 0]
-                (let [i-hs-combined (+ i-hs i-nd)]
-                  (if (= i-nd i-nd-max)
-                    i-nd
-                    (let [nd-val (nth needle i-nd)
-                          hs-val (nth haystack i-hs-combined)]
-                      (if (= nd-val hs-val)
-                        (recur (inc i-nd)))))))]
-          (if match-length
-            i-hs
-            (recur (+ i-hs (or match-length 1)))))))))
-
-;; #+pure
-(defn get-proper-subset-keys [base comp]
-  (filter (set (keys base))
-          (keys comp)))
-
-;; #+pure
-(defn is-submap? [smaller bigger]
-  (let [required-keys (get-proper-subset-keys smaller bigger)]
-    (and (= (count smaller)
-            (count required-keys))
-         (every? identity
-                 (map #(= (smaller %) (bigger %))
-                      required-keys)))))
-
-;; #+pure
 (defn get-next-state-list [transition-spec now-state now-world]
   (->> transition-spec
        (map (fn [[name next world]]
               (if (and (= now-state name)
-                       (is-submap? world now-world))
+                       (wk-coll/is-submap? world now-world))
                 next)))
        (filter identity)))
