aff304989039f21a7d577c64a0f5af7ee9cedadf
natto <whacked@users.noreply.github.com>
2018-10-21 09:51:14 -0700

    Add convenience fns to get cytograph node positions

--- src/cljs/wksymclj/ui/cytoscape.cljs
+++ src/cljs/wksymclj/ui/cytoscape.cljs
@@ -1,4 +1,6 @@
-(ns wksymclj.ui.cytoscape)
+(ns wksymclj.ui.cytoscape
+  (:require
+   [wksymclj.codec.cytoscape :as cyto-codec]))
 
 (defn add-node!
   "(add-node!
@@ -60,3 +62,21 @@
    (str "edge"
         "[source=\"" source-id "\"]"
         "[target=\"" target-id "\"]")))
+
+(defn get-node-position [node]
+  (if (map? node)
+    (let [pos (:position node)]
+      ;; :name or :id?
+      (assoc (select-keys pos [:x :y])
+             :name (get-in node [:data :name])))
+    ;; assume js cyto node object
+    (-> node
+        (js-invoke "position")
+        (js->clj :keywordize-keys true))))
+
+(defn get-graph-node-position-coll [cytograph-object]
+  (->> cytograph-object
+       (cyto-codec/cytoscape-graph-to-data)
+       (:elements)
+       (:nodes)
+       (map get-node-position)))
