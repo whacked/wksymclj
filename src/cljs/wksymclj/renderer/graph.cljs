afe573163458f7a6bcf6c1e0f571990b932a9629
natto <whacked@users.noreply.github.com>
2019-04-06 12:13:31 -0700

    Add WIP separate namespace for graph/flow-graph renderer

---
+++ src/cljs/wksymclj/renderer/graph.cljs
@@ -0,0 +1,122 @@
+(ns wksymclj.renderer.graph
+  (:require
+   [wksymclj.codec.graph :as graph-codec]
+   [wksymclj.ui.colorization :as color]
+   [wksymclj.ui.mxgraph :as mx
+    :refer [underscoreify-keys]]
+   [wksymclj.ui.cytoscape :as cyto]
+   [wksymclj.codec.cytoscape :as cyto-codec]
+   [cljs.nodejs :as nodejs]))
+
+(-> (nodejs/require "cytoscape")
+    (.use (nodejs/require "cytoscape-dagre")))
+
+(defn apply-style-map!
+  [graph-object style-map & elements]
+  (let [apply-func
+        (case (graph-codec/get-graph-type graph-object)
+          :cytograph
+          #(cyto/set-element-style! % style-map)
+    
+          :mxgraph
+          #(mx/set-style graph-object % style-map))]
+    (doseq [element elements]
+      (apply-func element))))
+
+(defn set-node-style-map!
+  [graph-object node-name style-map]
+  (case (graph-codec/get-graph-type graph-object)
+    :cytograph
+    ;; node-id "tw/tiddlers/software.tid"
+    (cyto/set-element-style!
+     (cyto/get-node graph-object node-name)
+     style-map)
+    
+    :mxgraph
+    ;; node-id "tw/tiddlers/software.tid"
+    (mx/set-style
+     graph-object
+     (mx/get-matching-node
+      graph-object
+      {:name node-name})
+     style-map)))
+
+(defn set-edge-style-map!
+  [graph-object pre-node-name post-node-name style-map]
+  (case (graph-codec/get-graph-type graph-object)
+    :cytograph
+    (cyto/set-element-style!
+     (cyto/get-edge
+      graph-object
+      pre-node-name post-node-name)
+     style-map)
+    
+    :mxgraph
+    (mx/set-style
+     graph-object
+     (mx/get-matching-edge
+      graph-object
+      pre-node-name post-node-name)
+     style-map)))
+
+(defn highlight-node-in-graph!
+  [graph-object node-name]
+  (set-node-style-map!
+   graph-object
+   node-name
+   (case (graph-codec/get-graph-type graph-object)
+     :cytograph
+     {"background-color" "yellow"
+      "border-width" "4px"
+      "border-color" "red"}
+
+     :mxgraph
+     {(aget mx/mxConstants "STYLE_FILLCOLOR") "yellow"
+      (aget mx/mxConstants "STYLE_STROKECOLOR") "red"
+      (aget mx/mxConstants "STYLE_STROKEWIDTH") 4})))
+
+(defn unhighlight-node-in-graph!
+  [graph-object node-name]
+  (set-node-style-map!
+   graph-object
+   node-name
+   (case (graph-codec/get-graph-type graph-object)
+     :cytograph
+     {"background-color" ""
+      "border-width" ""
+      "border-color" ""}
+
+     :mxgraph
+     {})))
+
+(defn highlight-edge-in-graph!
+  [graph-object pre-node-name post-node-name]
+  (set-edge-style-map!
+   graph-object
+   pre-node-name post-node-name
+   (case (graph-codec/get-graph-type graph-object)
+     :cytograph
+     {"width" "6"
+      "line-color" "yellow"
+      "target-arrow-color" "orange"}
+
+     ;; UNVERIFIED
+     :mxgraph
+     {(aget mx/mxConstants "STYLE_FILLCOLOR") "yellow"
+      (aget mx/mxConstants "STYLE_STROKECOLOR") "red"
+      (aget mx/mxConstants "STYLE_STROKEWIDTH") 4})))
+
+(defn unhighlight-edge-in-graph!
+  [graph-object pre-node-name post-node-name]
+  (set-edge-style-map!
+   graph-object
+   pre-node-name post-node-name
+   (case (graph-codec/get-graph-type graph-object)
+     :cytograph
+     {"width" "3"
+      "line-color" "gray"
+      "target-arrow-color" "green"}
+     
+     ;; UNVERIFIED
+     :mxgraph
+     {})))
