5aae16d098275a0a2e08da8a1fb452f0907ef72e
whacked <whacked@users.noreply.github.com>
2020-10-31 18:03:48 -0700

    substitute rehype-slug with prefix/postfix-aware renderer

--- src/cljs/wksymclj/codec/orgajs.cljs
+++ src/cljs/wksymclj/codec/orgajs.cljs
@@ -5,8 +5,41 @@
      ["unified" :as unified]
      ["orga-unified" :as orga-parse]
      ["orga-rehype" :as orga-ast-to-rehype-ast]
-     ["rehype-slug" :as add-slugs-to-headings]
+     
+     ;; rehype-slug substitute
+     ["github-slugger" :as github-slugger]
+     ["unist-util-visit" :as visit]
+     ["hast-util-to-string" :as toString]
+     ["hast-util-is-element" :as is]
+     ["hast-util-has-property" :as has]
+
      ["rehype-stringify" :as rehype-html]))
+
+(def $ORG-HEADING-ID-PREFIX
+  "org-header-")
+
+;; slug logic ref
+;; https://github.com/rehypejs/rehype-slug/blob/main/index.js except
+;; we want to qualify the slug, instead of creating an id
+;; directly from the text
+(def $headings #js ["h1" "h2" "h3" "h4" "h5" "h6"])
+
+(def $slugs (github-slugger))
+(defn add-slugs-to-headings [& [options]]
+  (let [heading-prefix (get options :prefix "")
+        heading-postfix (get options :postfix "")]
+    (fn transform [tree]
+      (.reset $slugs)
+      (visit tree
+             "element"
+             (fn [node]
+               (when (and (is node $headings)
+                          (not (has node "id")))
+                 (aset node "properties" "id"
+                       (.slug $slugs
+                              (str heading-prefix
+                                   (toString node)
+                                   heading-postfix)))))))))
 
 (defn preprocess-org-src
   "orga seems to have trouble with single links like [[target]];
@@ -25,7 +58,8 @@
   (let [processor (-> (unified)
                       (.use orga-parse)
                       (.use orga-ast-to-rehype-ast)
-                      (.use add-slugs-to-headings)
+                      (.use add-slugs-to-headings
+                            {:prefix $ORG-HEADING-ID-PREFIX})
                       (.use rehype-html))]
     (-> processor
         (.processSync
